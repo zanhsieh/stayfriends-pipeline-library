0ce0872abeececcda156e5d537c0522d57339782
Adrian Halamoda <adrian.halamoda@stayfriends.com>
2016-11-15 17:07:36 +0100

    more basic steps

---
+++ vars/sfDeployStaging.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/groovy
+
+def call() {
+
+	stage 'Deploy Staging' {
+		def utils = new io.fabric8.Utils()
+		def envStage = utils.environmentNamespace('staging')
+		kubernetesApply(environment: envStage)
+	}
+
+}
---
+++ vars/sfMavenBuildRelease.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/groovy
+
+
+def call() {
+
+    container(name: 'maven') {
+
+        stage 'Build Release' {
+
+            def versionPrefix = ""
+            try {
+              versionPrefix = VERSION_PREFIX
+            } catch (Throwable e) {
+              versionPrefix = "1.0"
+            }
+
+            def canaryVersion = "${versionPrefix}.${env.BUILD_NUMBER}"
+            mavenCanaryRelease {
+                version = canaryVersion
+            }
+        }
+    }
+
+}
