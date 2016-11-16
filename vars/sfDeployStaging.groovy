40f230612ae1a67707ab501e387cf3fdf2f360eb
Adrian Halamoda <adrian.halamoda@stayfriends.com>
2016-11-16 14:46:36 +0100

    dev

--- vars/sfDeployStaging.groovy
+++ vars/sfDeployStaging.groovy
@@ -5,13 +5,12 @@ def call(body) {
 	stage 'Deploy Staging' 
 		def utils = new io.fabric8.Utils()
 		def envStage = utils.environmentNamespace('staging')
-		echo "deploying to environment: " envStage
+		echo "deploying to environment: " + envStage
 
 		// this file is read as default, as it is produced by maven plugin f-m-p
 		rc = readFile file: "target/classes/META-INF/fabric8/kubernetes.yml"
 		echo "kubernetes rc = " + rc
 
 		kubernetesApply(environment: envStage)
-	
 
 }
