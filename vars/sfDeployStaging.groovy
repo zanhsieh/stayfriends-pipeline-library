8bf4bcc4970f04632f5dcb0b2d241d53af6ecb37
Adrian Halamoda <adrian.halamoda@stayfriends.com>
2016-11-17 16:07:13 +0100

    dev - deploy staging

--- vars/sfDeployStaging.groovy
+++ vars/sfDeployStaging.groovy
@@ -11,6 +11,13 @@ def call(body) {
     if ( !config.name ) {
     	config.name = env.JOB_NAME
     }
+    if ( !config.version ) {
+    	config.version = "0.${env.BUILD_NUMBER}"
+    }
+    if ( !config.image ) {
+		// default full image name including registry
+		config.image = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${env.KUBERNETES_NAMESPACE}/${config.name}:${config.version}"
+    }
     echo "deploy config = " + config
 
     //container(name: 'client') {
@@ -41,7 +48,7 @@ def call(body) {
 				container = config.container
 				version = config.version
 				port = 80
-				image = "${env.FABRIC8_DOCKER_REGISTRY_SERVICE_HOST}:${env.FABRIC8_DOCKER_REGISTRY_SERVICE_PORT}/${env.KUBERNETES_NAMESPACE}/${config.name}:${config.version}"
+				image = config.image
 				//icon = "https://cdn.rawgit.com/fabric8io/fabric8/dc05040/website/src/images/logos/nodejs.svg"
 			}
 		}
